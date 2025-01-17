a693722b7c43abdc72d0e8b4b19d5b1a4fbe7fc0
imsedhu <sedhuramansedhu17@gmail.com>
2025-01-17 20:53:58 +0530

    new commit

--- website-5/script.js
+++ website-5/script.js
@@ -4,3 +4,11 @@ const header = document.querySelector("header");
 toggle.addEventListener("click", ()=>{
   header.classList.toggle("active")
 })
+
+
+/* https://www.bolddesk.com/
+https://boldsign.com/
+https://www.boldreports.com/
+https://www.boldbi.com/
+
+BoldDesk (https://www.bolddesk.com/) */
